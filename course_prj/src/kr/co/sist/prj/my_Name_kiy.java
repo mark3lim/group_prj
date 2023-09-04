f6f9110f2b829e6a393527516ee0483b1c114f2e
user <user@DESKTOP-COJ9858>
2023-09-04 15:37:30 +0900

    임태균 바보

---
+++ course_prj/src/kr/co/sist/prj/my_Name_kiy.java
@@ -0,0 +1,13 @@
+package kr.co.sist.prj;
+
+import javax.swing.JOptionPane;
+
+public class my_Name_kiy {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "임태균 바보");
+		
+	}
+
+}
