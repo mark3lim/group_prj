84b741ce5e2f19c57622325ac564438affddadbe
user <user@DESKTOP-COJ9858>
2023-09-04 15:05:13 +0900

    test 함 해보실?

---
+++ course_prj/src/kr/co/sist/prj/test_inyoung.java
@@ -0,0 +1,11 @@
+package kr.co.sist.prj;
+
+import javax.swing.JOptionPane;
+
+public class test_inyoung {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "졸려용");
+	}
+
+}
