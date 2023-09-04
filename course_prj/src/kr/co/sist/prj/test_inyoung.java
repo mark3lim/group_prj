eff72d06fc9248f67acc51bd088483669264e5e5
user <user@DESKTOP-COJ9858>
2023-09-04 15:50:18 +0900

    야아아아아앙

--- course_prj/src/kr/co/sist/prj/test_inyoung.java
+++ course_prj/src/kr/co/sist/prj/test_inyoung.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class test_inyoung {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "졸려용??");
+		JOptionPane.showMessageDialog(null, "??졸려용??");
 	}
 
 }
