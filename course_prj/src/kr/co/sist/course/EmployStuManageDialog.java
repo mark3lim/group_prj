f14103d3285dc62fe9cd3e9091dca71855f9f985
user <user@DESKTOP-COJ9858>
2023-09-05 16:38:17 +0900

    stu

--- course_prj/src/kr/co/sist/course/EmployStuManageDialog.java
+++ course_prj/src/kr/co/sist/course/EmployStuManageDialog.java
@@ -30,33 +30,33 @@ public class EmployStuManageDialog extends JDialog {
 
 		setLayout(null);
 
-		// 타이틀
+		//타이틀
 		JLabel jlblTitle = new JLabel("학생 관리");
-		// 배경
+		//배경
 		JLabel jlblback = new JLabel(
 				new ImageIcon("C:/Users/user/git/group_prj/course_prj/src/kr/co/sist/prj/backgr.png"));
-		// 검색
+		//검색
 		DefaultComboBoxModel<String> dcbmSearch = new DefaultComboBoxModel<String>();
 		JComboBox<String> jcbSearch = new JComboBox<String>(dcbmSearch);
 		dcbmSearch.addElement("이름");
 		dcbmSearch.addElement("사번");
-		// 검색창
+		//검색창
 		JTextField jtfSearch = new JTextField();
-		// 등록 버튼
+		//등록 버튼
 		JButton jbtnAdd = new JButton("등록");
-		// 조회 버튼
+		//조회 버튼
 		JButton jbtnSearch = new JButton("조회");
-		// 테이블
+		//테이블
 		JTable jtStu = new JTable();
 
-		// setFont
+		//setFont
 		jlblTitle.setFont(new Font("Pretendard", Font.BOLD, 20));
 		jcbSearch.setFont(font);
 		jtfSearch.setFont(font);
 		jbtnSearch.setFont(font);
 		jbtnAdd.setFont(font);
 
-		// setBounds
+		//setBounds
 		jlblback.setBounds(0, 0, 900, 600);
 		jlblTitle.setBounds(95, 50, 210, 50);
 		jcbSearch.setBounds(470, 65, 100, 30);
@@ -64,7 +64,7 @@ public class EmployStuManageDialog extends JDialog {
 		jbtnSearch.setBounds(730, 65, 60, 30);
 		jbtnAdd.setBounds(700, 450, 80, 30);
 
-		// setBackground
+		//setBackground
 		jcbSearch.setBackground(new Color(0xE0E0E0));
 		jbtnSearch.setBackground(new Color(0xE0E0E0));
 		jbtnAdd.setBackground(new Color(0xE0E0E0));
@@ -72,14 +72,14 @@ public class EmployStuManageDialog extends JDialog {
 		jbtnSearch.setBorder(null);
 		jbtnAdd.setBorder(null);
 
-		// add
+		//add
 		add(jcbSearch);
 		add(jtfSearch);
 		add(jbtnSearch);
 		add(jlblTitle);
 		add(jbtnAdd);
 
-		// 배경
+		//배경
 		add(jlblback);
 
 		setBounds(main.getX() + 50, main.getY() + 50, 900, 600);
