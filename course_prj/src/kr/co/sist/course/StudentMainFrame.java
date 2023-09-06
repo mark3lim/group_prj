3b49da58a8d1e68c0b77881037b5827494d62913
user <user@DESKTOP-COJ9858>
2023-09-06 12:35:38 +0900

    학생 메인 화면 틀과 버튼 구현

--- course_prj/src/kr/co/sist/course/StudentMainFrame.java
+++ course_prj/src/kr/co/sist/course/StudentMainFrame.java
@@ -1,7 +1,11 @@
 package kr.co.sist.course;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -29,29 +33,105 @@ public class StudentMainFrame extends JFrame {
 	public StudentMainFrame(StudentVO stuVO) {
 		sVO = stuVO;
 		
-		jlblMyPhoto = new JLabel();
-		jlblMyPhoto.setBounds(100, 100, 200, 250);
-		jlblMyPhoto.setBorder(new LineBorder(Color.red));
-		
+		//배경 사진 설정
 		JLabel jlblBg = new JLabel(new ImageIcon("C:/Users/user/git/group_prj/course_prj/src/kr/co/sist/course/images/mainBg.png"));
 		jlblBg.setBounds(0, 0, 1200, 800);
-
-		JPanel pnlButton = new JPanel();
-		pnlButton.setBounds(500, 100, 400, 400);
+		
+		//학생 사진을 보여주는 라벨 설정
+		jlblMyPhoto = new JLabel();
+		jlblMyPhoto.setBounds(220, 150, 200, 250);
+		jlblMyPhoto.setBorder(new LineBorder(Color.red));
+		
+		//메인 버튼들을 모아두는 패널과 버튼들 설정
+		JPanel pnlButton = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 60));
+		pnlButton.setBounds(620, 170, 400, 400);
 		pnlButton.setBorder(new LineBorder(Color.red));
+		Dimension d = new Dimension(300, 50);
+		
+		jbtnRegisterSubject = new JButton("수강과목");
+		jbtnRegisterSubject.setPreferredSize(d);
+		jbtnGradeSearch = new JButton("성적조외");
+		jbtnGradeSearch.setPreferredSize(d);
+		jbtnCourseApply = new JButton("수강신청");
+		jbtnCourseApply.setPreferredSize(d);
+		
+		pnlButton.add(jbtnRegisterSubject);
+		pnlButton.add(jbtnGradeSearch);
+		pnlButton.add(jbtnCourseApply);
+		
+		//로그인하면 보여주는 학생 정보를 보여주는 패널과 라벨 설정
+		JPanel pnlInfoTag = setInfoLable(); //분류 라벨 만드는 method
+		pnlInfoTag.setLocation(jlblMyPhoto.getX(), jlblMyPhoto.getY()+290);
+		
+		jlblDept = new JLabel("컴퓨터공학부");
+		jlblDept.setBounds(100, 0, 200, 30);
+		jlblMajor = new JLabel("컴퓨터공학");
+		jlblMajor.setBounds(100, 35, 200, 30);
+		jlblEmail = new JLabel("test@gmail.com");
+		jlblEmail.setBounds(100, 70, 200, 30);
+		jlbleLoginTime = new JLabel("2023.09.06 12:01:43");
+		jlbleLoginTime.setBounds(100, 105, 200, 30);
+		
+		pnlInfoTag.add(jlblDept);
+		pnlInfoTag.add(jlblMajor);
+		pnlInfoTag.add(jlblEmail);
+		pnlInfoTag.add(jlbleLoginTime);
+		
+		//이름, 내 정보, 로그아웃이 모여있는 패널과 설정
+		JPanel pnlName = new JPanel();
+		pnlName.setBounds(740, jlblMyPhoto.getY()-45, 280, 35);
+		pnlName.setLayout(null);
+		
+		Font font = new Font("맑은 고딕", Font.BOLD, 16);
+		
+		jlblMyName = new JLabel("농담곰", JLabel.CENTER);
+		jlblMyName.setBounds(0, 2, 80, 30);
+		jlblMyName.setBorder(new LineBorder(Color.pink));
+		jlblMyName.setFont(font);
+		
+		jbtnMyProfile = new JButton("내 정보");
+		jbtnMyProfile.setBounds(90, 5, 85, 25);
+		jbtnLogout = new JButton("로그아웃");
+		jbtnLogout.setBounds(190, 5, 85, 25);
+		
+		pnlName.add(jlblMyName);
+		pnlName.add(jbtnMyProfile);
+		pnlName.add(jbtnLogout);
+		
 		
 		setLayout(null);
 		
-		add(jlblBg);
 		add(jlblMyPhoto);
 		add(pnlButton);
+		add(pnlInfoTag);
+		add(pnlName);
 		
-		
-//		add(pnlBg);
+		add(jlblBg);
 		
 		setBounds(200, 150, 1200, 800);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public JPanel setInfoLable() {
+		String[] strArr = {"학부", "학과", "이메일", "로그인 시간"};
+		JLabel[] jl = new JLabel[strArr.length];
+		Font font = new Font(null, Font.BOLD, 14);
+		
+		JPanel pnl = new JPanel();
+		pnl.setLayout(null);
+		pnl.setSize(310, 140);
+		
+		for(int i = 0; i < jl.length; i++) {
+			jl[i] = new JLabel(strArr[i]);
+			jl[i].setBounds(10, 35*i, 80, 30);
+			jl[i].setFont(font);
+			
+			pnl.add(jl[i]);
+		}
+		
+		
+		return pnl;
 	}
 
 	public static void main(String[] args) {
